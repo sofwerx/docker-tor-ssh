a587f55499fec8e94b19df22d179186e4c5b2825
Ian Blenke <ian@blenke.com>
2018-03-01 22:30:16 -0500

    Adding support for OTHER_PORTS

--- run.sh
+++ run.sh
@@ -13,13 +13,20 @@ sed -i -e 's@^#%include /etc/torrc.d/@%include /etc/torrc.d/@' /etc/tor/torrc
 mkdir -p /etc/torrc.d
 
 cat <<EOF > /etc/torrc.d/ssh
+SocksPort 0
+SafeLogging 1
+
 HiddenServiceDir /var/lib/tor/ssh/
+
 HiddenServiceVersion 3
 HiddenServicePort 22 ${ssh_host_ip}:${ssh_host_port}
-
-SafeLogging 0
-Log notice stdout
 EOF
+
+for port in "${OTHER_PORTS}" ; do
+  cat <<EOF >> /etc/torrc.d/ssh
+HiddenServicePort $port ${ssh_host_ip}:$port
+EOF
+done
 
 echo "Configuration:"
 grep -v -e '^#\|^$' /etc/tor/torrc /etc/torrc.d/*
